9507ff49b4e66c90899636ed68eb962458a226b2
David Cesiel <dcesiel@gmail.com>
2013-09-21 02:53:46 -0400

    Removed uneeded files

--- app/src/com/dcesiel/wewrite/MainActivity.java
+++ app/src/com/dcesiel/wewrite/MainActivity.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.EditText;
 import edu.umich.imlc.collabrify.client.CollabrifyClient;
 import edu.umich.imlc.collabrify.client.CollabrifyListener;
 import edu.umich.imlc.collabrify.client.CollabrifyParticipant;
@@ -12,11 +14,19 @@ import edu.umich.imlc.collabrify.client.CollabrifySession;
 import edu.umich.imlc.collabrify.client.exceptions.CollabrifyException;
 
 public class MainActivity extends Activity {
+  
+  public static final String SESSION_NAME = "cesiel";
+  public static final String SESSION_TAGS = "";
+  
+  private EditText editText;
+  
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        editText = (EditText) findViewById(R.id.textBox);
         try
         {
           CollabrifyListener collabrifyListener = new CollabrifyListener()
@@ -40,6 +50,7 @@ public class MainActivity extends Activity {
             public void onSessionCreated(long id)
             {
               // TODO Auto-generated method stub
+              Log.i("", "Session Created");
               
             }
             
@@ -109,17 +120,27 @@ public class MainActivity extends Activity {
           };
           CollabrifyClient collabClient = new CollabrifyClient(this, "dcesiel@gmail.com", "dcesiel", 
                             "441fall2013@umich.edu", "XY3721425NoScOpE", true, collabrifyListener);
+          collabClient.createSession(SESSION_NAME, null, null, 0);
         }
         catch(CollabrifyException e){
           e.printStackTrace();
         }
+        
+        
+        
+        
     }
 
-
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+        
+        
+        
         return true;
     }
     
