8b0a99b62c70f9bf67dbae96e069de96c243598f
David Cesiel <dcesiel@gmail.com>
2013-09-21 06:11:36 -0400

    Added undo and redo click listeners to MainActivity

--- app/src/com/dcesiel/wewrite/MainActivity.java
+++ app/src/com/dcesiel/wewrite/MainActivity.java
@@ -1,11 +1,15 @@
 package com.dcesiel.wewrite;
 
+import java.util.Stack;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
@@ -14,17 +18,29 @@ public class MainActivity extends Activity {
   public static final String SESSION_TAGS = "";
   
   private EditText text;
-  
+  //private Stack<UndoRedoData> undoRedoStack;
+  private Button undo;
+  private Button redo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        //undoRedoStack = new Stack<UndoRedoData>();
+        
         text = (EditText) findViewById(R.id.textBox);
+        undo = (Button) findViewById(R.id.undo);
+        redo = (Button) findViewById(R.id.redo);
+        
+        undo.setOnClickListener(undoButtonHandler);
+        redo.setOnClickListener(redoButtonListner);
+        
+        
         text.addTextChangedListener(new TextWatcher(){
             public void onTextChanged(CharSequence s, int start, int before, int count){
-              Log.i("TextBuffer", s.toString() + " " + Integer.toString(start) + " " + Integer.toString(count));
+              Log.i("TextBuffer", "Char: " + " " + s.charAt(start) + " Value: " + start);
+              //undoRedoStack.push(new UndoRedoData(s.charAt(start), start));
             }
 
             @Override
@@ -45,6 +61,18 @@ public class MainActivity extends Activity {
         
         
     }
+    
+    View.OnClickListener undoButtonHandler = new View.OnClickListener() {
+      public void onClick(View v) {
+        Log.i("Undo Button:", "Undo Pressed");
+      }
+    };
+    
+    View.OnClickListener redoButtonListner = new View.OnClickListener() {
+      public void onClick(View v) {
+        Log.i("Redo Button:", "Redo Pressed");
+      }
+    };
 
     
     
